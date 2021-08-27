49025a502678cecada90fddd8f9b718c9ae8b164
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-27 00:35:28 -0500

    state property for count number

--- DataFlow/ContentView.swift
+++ DataFlow/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: -Properties 
+    @State var count = 0
+    
+    //MARK: - Body
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
+
+//MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
